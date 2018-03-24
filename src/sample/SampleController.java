0cb047898a93fc03567cc7f52c75d436bb3418a4
Luca Semprini <luca.semprini10@studio.unibo.it>
2018-03-24 17:01:39 +0100

    Collegata la Matrix al Controller!

--- src/sample/SampleController.java
+++ src/sample/SampleController.java
@@ -1,13 +1,28 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import model.Matrix;
+import model.MatrixImpl;
+import view.GameCell;
 
-public class SampleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SampleController implements Initializable {
+
+    private static final int NUMCOLUMNS = 100;
+    private static final int NUMROWS = 100;
+    private static final int CELL_DIMENSION = 10;
+
+    private final Matrix myMatrix;
 
     @FXML
     public ScrollPane scrollPane;
@@ -21,19 +36,47 @@ public class SampleController {
     public Label generationsLabel;
     @FXML
     public AnchorPane anchorPane;
+    @FXML
+    public BorderPane buttonPane;
 
     public SampleController() {
+        this.myMatrix = new MatrixImpl(NUMROWS, NUMCOLUMNS);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         this.setResizeOptions();
+        this.setGridPane(NUMROWS, NUMCOLUMNS);
+        this.setScrollPane();
     }
 
     @FXML
     private void setResizeOptions() {
         this.scrollPane.prefHeightProperty().bind(anchorPane.heightProperty());
         this.scrollPane.prefWidthProperty().bind(anchorPane.widthProperty());
+        this.buttonPane.prefWidthProperty().bind(anchorPane.widthProperty());
     }
 
-    public void setGridPane(final int numRows, final int numColumns) {
-        //TODO settare il numero di righe e colonne della griglia.
+    @FXML
+    private void setGridPane(final int numRows, final int numColumns) {
+
+        ColumnConstraints colConst = new ColumnConstraints();
+        RowConstraints rowConst = new RowConstraints();
+        rowConst.setPercentHeight(100.0 / numRows);
+        colConst.setPercentWidth(100.0 / numColumns);
+
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numColumns; col++) {
+                GameCell cell = new GameCell(CELL_DIMENSION, CELL_DIMENSION, myMatrix.getCellAt(row, col).getCurrentState());
+                gameGrid.add(cell, col, row);
+            }
+        }
+        for (int i = 0; i < numColumns; i++) {
+            gameGrid.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < numRows; i++) {
+            gameGrid.getRowConstraints().add(rowConst);
+        }
     }
 
     public void addCanvasOnGrid() {
@@ -44,11 +87,17 @@ public class SampleController {
         //TODO Aggiungi onClickListeners ai due bottoni.
      }
 
-     public void setScrollPane() {
-        //TODO configura lo ScrollPane.
+     @FXML
+     private void setScrollPane() {
+        this.scrollPane.setContent(this.gameGrid);
+        this.scrollPane.setFitToWidth(true);
+        this.scrollPane.setFitToHeight(true);
+        this.scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+         this.scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
      }
 
      public void updateLabelGenerations() {
         //TODO Aggiorna la Label delle generazioni.
      }
+
 }
